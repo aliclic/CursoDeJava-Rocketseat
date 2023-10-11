eadec43d179771d25a3631b3b58c4c01a2c64615
aliclic <alicvictor11@gmail.com>
2023-10-11 20:35:54 -0300

    aula 03

---
+++ src/main/java/br/com/aliclic/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,61 @@
+package br.com.aliclic.todolist.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.aliclic.todolist.user.IUserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        // Pegar a autenticação (usario e senha)
+        var authorization = request.getHeader("Authorization"); // Basic QWxpY2xpYzoxMjM=
+
+        var authEncoded = authorization.substring("Basic".length()).trim(); // QWxpY2xpYzoxMjM=
+
+        byte[] authDecode = Base64.getDecoder().decode(authEncoded); // [B@6f46a25d
+        var authString = new String(authDecode); // Aliclic:123
+
+        String[] credentials = authString.split(":");
+        String username = credentials[0];
+        String password = credentials[1];
+
+        System.out.println("Authorization");
+        System.out.println(username);
+        System.out.println(password);
+
+        // Validar usuário
+        var user = this.userRepository.findByUsername(username);
+        if(user == null) {
+            response.sendError(401);
+        } else {
+            // Validar senha
+            var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+            if(passwordVerify.verified) {
+                // Segue viagem
+                filterChain.doFilter(request, response);
+            } else {
+                response.sendError(401);
+            }
+        }
+    
+    }
+
+    
+}
